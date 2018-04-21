60cb6c8d3f608fe754d8b10eb5fcf3e7e9cfa599
varunsridhar1 <varun.sridhar@utexas.edu>
2018-04-21 11:20:58 -0500

    Scraped top post titles

---
+++ scrape_post.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 21 11:04:33 2018
+
+@author: Varun
+"""
+
+import json, requests
+
+subreddit = 'askreddit'
+
+r = requests.get(
+    'http://www.reddit.com/r/{}.json'.format(subreddit),
+    headers={'user-agent': 'Mozilla/5.0'}
+)
+
+# view structure of an individual post
+# print(json.dumps(r.json()['data']['children'][0]))
+
+for post in r.json()['data']['children']:
+    print(post['data']['title'])
